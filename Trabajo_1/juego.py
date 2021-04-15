454a4977c9e09bd022445cf73baab5771a1fa7dc
SebasED <johns.estrada@udea.edu.co>
2021-04-15 17:22:12 -0500

    Últimos detalles a los comentarios

--- Trabajo_1/juego.py
+++ Trabajo_1/juego.py
@@ -1,6 +1,6 @@
 # ----------Conceptos básicos de PDI----------------------------
-# ----------Por: John Sebastian Estrada Durango. johns.estrada@udea.edu.co CC 1037502955 --------------------------
-# -----------Daniel Steve Blandon -----------------------------------
+# ----------Por: John Sebastian Estrada Durango. johns.estrada@udea.edu.co  --------------------------
+# -----------Daniel Steve Blandón Sánchez.  Steve.blandon@udea.edu.co-----------------------------------
 # -----------Curso de Procesamiento Digital de Imágenes ------------------------------------------------------
 # ---------- 19 Abril de 2021 --------------------------------------------------------------------------------
 
@@ -92,7 +92,7 @@ class Explosion(pygame.sprite.Sprite):
 
 # Clase que representa las balas que lanzara la nave
 class Bullet(pygame.sprite.Sprite):
-    # Método cpara iniciar la clase Bullet, de donde y hacia donde debe ir
+    # Método para iniciar la clase Bullet, de donde y hacia donde debe ir
     def __init__(self, x0, y0, xf, yf):
         super().__init__()
         self.image = pygame.image.load("assets/laser1.png")
@@ -122,39 +122,27 @@ class Bullet(pygame.sprite.Sprite):
 
 # Método para dibujar en un frame las mascaras de los colores detectados
 def dibujar(mask, color):
-    # Se obtienen los contornos de las imagenes que entren en el rango del color definido
     contornos, _ = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # for para eliminar los contornos que hacen ruido y dejar el que necesitamos
-    for c in contornos:
+        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)# Se obtienen los contornos de las imagenes que entren en el rango del color definido
+    for c in contornos:# for para eliminar los contornos que hacen ruido y dejar el que necesitamos
         area = cv2.contourArea(c)  # Encuentra el área del contorno
-        if area > 3000:
+        if area > 900:
             M = cv2.moments(c)  # función para encontrar el centro del área
-            if (M["m00"] == 0):
-                M["m00"] = 1  # se asigna 1 en caso que sea cero para la división
-            # Se identifica el punto x del centro del área
-            x = int(M["m10"]/M["m00"])
-            # Se identifica el punto y del centro del área
-            y = int(M["m01"]/M["m00"])
-            # Indicar si el color es azul para llevarle las posiciones del centro del área al Player
-            if color == (255, 0, 0):
+            if (M["m00"] == 0):M["m00"] = 1  # se asigna 1 en caso que sea cero para la división
+            x = int(M["m10"]/M["m00"])# Se identifica el punto x del centro del área
+            y = int(M["m01"]/M["m00"])# Se identifica el punto y del centro del área
+            if color == (255, 0, 0):# Indicar si el color es azul para llevarle las posiciones del centro del área al Player
                 player.rect.centerx = x
                 player.rect.centery = y
-            else:  # Si es otro color, se pasan las posiciones a donde deben ir las balas
-                player.shoot(x, y)
-            # Mejora el contorno, lo suabisa eliminando los picos
-            nuevoContorno = cv2.convexHull(c)
-            # Se dibuja un circulo en el frame con los puntos del centro del área
-            cv2.circle(frame, (x, y), 7, (0, 255, 0), -1)
+            else:player.shoot(x, y)# Si es otro color, se pasan las posiciones a donde deben ir las balas
+            nuevoContorno = cv2.convexHull(c)# Mejora el contorno, lo suabisa eliminando los picos
+            cv2.circle(frame, (x, y), 7, (0, 255, 0), -1)# Se dibuja un circulo en el frame con los puntos del centro del área
             font = cv2.FONT_HERSHEY_SIMPLEX  # Se asigna una fuente
             cv2.putText(frame, '{},{}'.format(x, y), (x+10, y), font, 0.75,
                         (0, 255, 0), 1, cv2.LINE_AA)  # Se pone un texto en el frame
-            # Dibujar el contorno de los objetos capturados en el frame
-            cv2.drawContours(frame, [nuevoContorno], 0, color, 3)
+            cv2.drawContours(frame, [nuevoContorno], 0, color, 3)# Dibujar el contorno de los objetos capturados en el frame
 
 # Método de la pantalla principal con las instrucciones del juego
-
-
 def show_go_screen():
     screen.blit(background, [0, 0])  # Pone una imagen de fondo
     draw_text(screen, "SPACE GAME", 65, WIDTH // 2,
@@ -181,24 +169,15 @@ def show_go_screen():
 # -----------------------------------------------------------------------------------------------------------
 # 3. INICIALIZAR VARIABLES
 # -----------------------------------------------------------------------------------------------------------
-
-
-# Captura la imagen de nuestra camara, streaming
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)# Captura la imagen de nuestra camara, streaming
 WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Dimensiones de campo de juego
-# Dimensiones de campo de juego
-HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))# Dimensiones de campo de juego
 BLACK = (0, 0, 0)  # Definir un color
-
 pygame.init()  # Iniciar la librería paygame para el juego
 pygame.mixer.init()  # Iniciar la librería paygame para el juego
-# Pasar dimensiones a la pantalla donde se mostrará el juego
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# Mostrar nombre del juego en la pantalla
-pygame.display.set_caption("SPACE GAME")
-# clock que controlara las velocidades a las que se moveran los objetos en el juego
-clock = pygame.time.Clock()
-
+screen = pygame.display.set_mode((WIDTH, HEIGHT))# Pasar dimensiones a la pantalla donde se mostrará el juego
+pygame.display.set_caption("SPACE GAME")# Mostrar nombre del juego en la pantalla
+clock = pygame.time.Clock()# clock que controlara las velocidades a las que se moveran los objetos en el juego
 meteor_images = []
 meteor_list = ["assets/meteorGrey_big1.png", "assets/meteorGrey_big2.png", "assets/meteorGrey_big3.png", "assets/meteorGrey_big4.png",
                "assets/meteorGrey_med1.png", "assets/meteorGrey_med2.png", "assets/meteorGrey_small1.png", "assets/meteorGrey_small2.png",
@@ -214,48 +193,28 @@ for i in range(9):  # Agregar las imagenes que representan las explosiones
     img_scale = pygame.transform.scale(img, (70, 70))
     explosion_anim.append(img_scale)
 
-# Asignar una imagen al fondo de pantalla
-background = pygame.image.load("assets/background.png").convert()
+background = pygame.image.load("assets/background.png").convert()# Asignar una imagen al fondo de pantalla
 
-# Rango bajo del color amarillo
-amarilloBajo = np.array([15, 100, 20], np.uint8)
-# Rango alto del color amarillo
-amarilloAlto = np.array([45, 255, 255], np.uint8)
+amarilloBajo = np.array([15, 100, 20], np.uint8)# Rango bajo del color amarillo
+amarilloAlto = np.array([45, 255, 255], np.uint8)# Rango alto del color amarillo
 azulBajo = np.array([100, 100, 20], np.uint8)  # Rango bajo del color azul
 azulAlto = np.array([125, 255, 255], np.uint8)  # Rango alto del color azul
-# redBajo1 = np.array([0, 100, 20], np.uint8)# Rangos bajos del color que se desea capturar
-# redAlto1 = np.array([0, 255, 255], np.uint8)# Rangos altos del color que se desea capturar
-# redBajo2 = np.array([173, 100, 20], np.uint8)# Rangos bajos del color que se desea capturar
-# redAlto2 = np.array([179, 255, 255], np.uint8)# Rangos altos del color que se desea capturar
 
 game_over = True  # Variable para controlar el fin y el inicio del juego
 running = True  # Variable para definir cuando el juego este corriendo
 
 # -----------------------------------------------------------------------------------------------------------
-# 3. EJECUCIÓN DEL JUEGO
+# 4. EJECUCIÓN DEL JUEGO
 # -----------------------------------------------------------------------------------------------------------
-
 while running:  # ciclo principal del juego
-
     ret, frame = cap.read()  # Capturar la imagen de la camara
-    # Función espejo para los movimientos se tomen acordes a los que hace el jugador frente a la camara
-    frame = cv2.flip(frame, 1)
+    frame = cv2.flip(frame, 1)# Función espejo para los movimientos se tomen acordes a los que hace el jugador frente a la camara
     if ret == True:  # Cerificar que se este capturando una imagen
-        # convierte la imagen (RGB) que captura el frame a formato HSV
-        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # Determinar el rango para el colore azul, imagen binarizada
-        maskAzul = cv2.inRange(frameHSV, azulBajo, azulAlto)
-        # Determinar el rango para el colore amarillo, imagen binarizada
-        maskAmarillo = cv2.inRange(frameHSV, amarilloBajo, amarilloAlto)
-        # maskRojo_1 = cv2.inRange(frameHSV, redBajo1,redAlto1)# Determinar el primer rango para el colore rojo
-        # maskRojo_2 =cv2.inRange(frameHSV, redBajo2, redAlto2)# Determinar el segundo rango para el colore rojo
-        # maskRojo = cv2.add(maskRojo_1,maskRojo_2)# Determinar el rango para el color rojo, imagen binarizada
-        # función para mostrar en pantalla lo que captura la camara en el rango y color determinado
-        dibujar(maskAzul, (255, 0, 0))
-        # función para mostrar en pantalla lo que captura la camara en el rango y color determinado
-        dibujar(maskAmarillo, (0, 255, 255))
-        # dibujar(maskRojo, (0, 0, 255))# función para mostrar en pantalla lo que captura la camara en el rango y color determinado
-        # cv2.imshow('maskAzul', mask)# Mostrar imagen binarizada
+        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)# convierte la imagen (RGB) que captura el frame a formato HSV
+        maskAzul = cv2.inRange(frameHSV, azulBajo, azulAlto)# Determinar el rango para el colore azul, imagen binarizada
+        maskAmarillo = cv2.inRange(frameHSV, amarilloBajo, amarilloAlto) # Determinar el rango para el colore amarillo, imagen binarizada
+        dibujar(maskAzul, (255, 0, 0))# función para mostrar en pantalla lo que captura la camara en el rango y color determinado
+        dibujar(maskAmarillo, (0, 255, 255))# función para mostrar en pantalla lo que captura la camara en el rango y color determinado
         cv2.imshow('frame', frame)  # mostrar el frame
         if cv2.waitKey(1) & 0xFF == ord('s'):  # condición para terminar la ejecución
             break
@@ -263,31 +222,24 @@ while running:  # ciclo principal del juego
     if game_over:  # Condición para la ventana de inicio
         show_go_screen()  # Llamar a la función para iniciar el juego o terminarlo
         game_over = False
-        # Lista para guardar todos los objetos del juego
-        all_sprites = pygame.sprite.Group()
+        all_sprites = pygame.sprite.Group()# Lista para guardar todos los objetos del juego
         meteor_list = pygame.sprite.Group()  # Lista para guardar los meteoritos de juego
         bullets = pygame.sprite.Group()  # Lista para guardar las balas de juego
         player = Player()  # instanciar un Player
-        # Agregar el Player a la lista de sprites y objetos
-        all_sprites.add(player)
-
+        all_sprites.add(player)# Agregar el Player a la lista de sprites y objetos
         for i in range(8):  # Crear los meteoritos y a gregarlos a las listas correpondietnes
             meteor = Meteor()
             all_sprites.add(meteor)
             meteor_list.add(meteor)
 
         score = 10  # Valor en que iniciara el score
-
     clock.tick(60)
     for event in pygame.event.get():  # Capturar los eventos
-        # check for closing window
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # terminar la ejecución si se cumple la condición
             running = False
 
-    all_sprites.update()  # Actualizar todos los sprites u objetos del juego
-
-    # Capturar las colisiones entre metoritos y las balas
-    hits = pygame.sprite.groupcollide(meteor_list, bullets, True, True)
+    all_sprites.update()  # Actualizar todos los sprites u objetos del juego    
+    hits = pygame.sprite.groupcollide(meteor_list, bullets, True, True)# Capturar las colisiones entre metoritos y las balas
     for hit in hits:  # Instanciar las explosiones de acuerdo a los choques entre meteoritos y balas
         explosion = Explosion(hit.rect.center)
         all_sprites.add(explosion)
@@ -295,26 +247,21 @@ while running:  # ciclo principal del juego
         all_sprites.add(meteor)
         meteor_list.add(meteor)
 
-    # Capturar las colisiones entre metoritos y la nave
-    hits = pygame.sprite.spritecollide(player, meteor_list, True)
+    hits = pygame.sprite.spritecollide(player, meteor_list, True)# Capturar las colisiones entre metoritos y la nave
     for hit in hits:  # Recorre las colisiones
         score -= 1  # Resta un punto al score
         meteor = Meteor()  # Crea el meteorito destruido por el choque
         all_sprites.add(meteor)
         meteor_list.add(meteor)
-        # Crea la explosión que genera el impacto
-        explosion = Explosion(hit.rect.center)
+        explosion = Explosion(hit.rect.center)# Crea la explosión que genera el impacto
         all_sprites.add(explosion)
         if score == 0:  # Condición para saber si se acabaron las vidas y el juego inica o termina
             game_over = True
-
-    # Dibuja el fondo de pantalla en las cordenadas [0,0]
-    screen.blit(background, [0, 0])
+    
+    screen.blit(background, [0, 0])# Dibuja el fondo de pantalla en las cordenadas [0,0]
     all_sprites.draw(screen)  # Dibuja todos los sprites u objetos
-    # Dibuja el score en pantalla
-    draw_text(screen, str(score), 25, WIDTH // 2, 10)
+    draw_text(screen, str(score), 25, WIDTH // 2, 10)# Dibuja el score en pantalla
     pygame.display.flip()  # muestra los cambios actualizados
-
 pygame.quit()  # Finaliza el pygame
 cap.release()
 cv2.destroyAllWindows()  # Finaliza y termina todas las ventanas
